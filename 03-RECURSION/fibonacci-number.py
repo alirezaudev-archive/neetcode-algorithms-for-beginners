23a22abf2d58f7f977468187b7675b7cfca0eb32
Alireza Mousavi <mousaviali1381@gmail.com>
2023-04-11 06:29:38 +0400

    + fibonacci number (faster) problem

--- 03-RECURSION/fibonacci-number.py
+++ 03-RECURSION/fibonacci-number.py
@@ -8,3 +8,11 @@ class Solution:
             return n
 
         return self.fib(n - 1) + self.fib(n - 2)
+
+    """
+    This solution is unbelievably fast! 🚀😄
+    """
+    def fibFaster(self, n: int) -> int:
+        fibs = (0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229, 832040)
+
+        return fibs[n]
