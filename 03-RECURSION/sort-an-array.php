d268dc49a39c31cb0012dce34f21335bc9c26eab
Alireza Mousavi <mousaviali1381@gmail.com>
2023-04-14 08:27:40 +0400

    + merge sorting algorithm for sort an array problem

--- 03-RECURSION/sort-an-array.php
+++ 03-RECURSION/sort-an-array.php
@@ -1,12 +1,12 @@
 <?php
 
 # https://leetcode.com/problems/sort-an-array/
-class Solution
+class InsertionSortingSolution
 {
     /**
      * Insertion Sorting Algorithm
      */
-    public function insertionSortArray(array $nums): array
+    public function sortArray(array $nums): array
     {
         for ($i = 1; $i < count($nums); $i++) {
             $j = $i - 1;
@@ -21,3 +21,62 @@ class Solution
         return $nums;
     }
 }
+class MergeSortingSolution
+{
+    /**
+     * Merge Sorting Algorithm
+     */
+    public function sortArray(array $nums): array
+    {
+        return $this->mergeSort($nums, 0, count($nums) - 1);
+    }
+
+    private function mergeSort(array $nums, int $start, int $end): array
+    {
+        if ($end - $start + 1 <= 1) {
+            return array_slice($nums, $start, $end - $start + 1);
+        }
+
+        $middle = (int)(($start + $end) / 2);
+
+        $leftSorted = $this->mergeSort($nums, $start, $middle);
+        $rightSorted = $this->mergeSort($nums, $middle + 1, $end);
+
+        return $this->merge($leftSorted, $rightSorted);
+    }
+
+    private function merge(array $left, array $right): array
+    {
+        $merged = [];
+        $i_left = $i_right = 0;
+
+        while ($i_left < count($left) && $i_right < count($right)) {
+            if ($left[$i_left] <= $right[$i_right]) {
+                $merged[] = $left[$i_left];
+                $i_left++;
+            } else {
+                $merged[] = $right[$i_right];
+                $i_right++;
+            }
+        }
+
+        while ($i_left < count($left)) {
+            $merged[] = $left[$i_left];
+            $i_left++;
+        }
+
+        while ($i_right < count($right)) {
+            $merged[] = $right[$i_right];
+            $i_right++;
+        }
+
+        return $merged;
+    }
+}
+
+$nums = [2, 3, 4, 1, 6];
+$insertionSorted = (new InsertionSortingSolution())->sortArray($nums);
+$mergeSorted = (new MergeSortingSolution())->sortArray($nums);
+
+print_r($insertionSorted);
+print_r($mergeSorted);
--- 03-RECURSION/sort-an-array.py
+++ 03-RECURSION/sort-an-array.py
@@ -2,8 +2,9 @@ from typing import *
 
 
 # https://leetcode.com/problems/sort-an-array/
-class Solution:
-    def insertionSortArray(self, nums: List[int]) -> List[int]:
+class InsertionSortingSolution:
+
+    def sortArray(self, nums: List[int]) -> List[int]:
         """
         Insertion Sorting Algorithm
         """
@@ -14,3 +15,46 @@ class Solution:
                 j -= 1
 
         return nums
+
+
+class MergeSortingSolution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        """
+        Merge Sorting Algorithm
+        """
+        return self.__mergeSort(nums, 0, len(nums))
+
+    def __mergeSort(self, nums: List[int], start: int, end: int) -> List[int]:
+        if end - start + 1 <= 1:
+            return nums[start:end + 1]
+
+        middle = (start + end) // 2
+
+        leftSorted = self.__mergeSort(nums, start, middle)
+        rightSorted = self.__mergeSort(nums, middle + 1, end)
+
+        return self.__merge(leftSorted, rightSorted)
+
+    def __merge(self, left: List[int], right: List[int]) -> List[int]:
+        merged = []
+        i_left = i_right = 0
+
+        while i_left < len(left) and i_right < len(right):
+            if left[i_left] <= right[i_right]:
+                merged.append(left[i_left])
+                i_left += 1
+            else:
+                merged.append(right[i_right])
+                i_right += 1
+
+        merged.extend(left[i_left:])
+        merged.extend(right[i_right:])
+
+        return merged
+
+
+insertionSorted = InsertionSortingSolution().sortArray([2, 3, 4, 1, 6])
+mergeSorted = MergeSortingSolution().sortArray([2, 3, 4, 1, 6])
+
+print(insertionSorted)
+print(mergeSorted)
