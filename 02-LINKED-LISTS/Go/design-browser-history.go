8a3035324f4cd28bc832e75488312b9e2dc2b221
Alireza Mousavi <mousaviali1381@gmail.com>
2025-02-28 00:51:52 +0330

    + design browser history (go solution)

---
+++ 02-LINKED-LISTS/Go/design-browser-history.go
@@ -0,0 +1,42 @@
+package main
+
+type Page struct {
+	url  string
+	next *Page
+	prev *Page
+}
+
+type BrowserHistory struct {
+	current *Page
+}
+
+func constructor(homepage string) BrowserHistory {
+	return BrowserHistory{
+		current: &Page{url: homepage},
+	}
+}
+
+func (this *BrowserHistory) Visit(url string) {
+	newPage := &Page{url: url}
+	newPage.prev = this.current
+	this.current.next = newPage
+	this.current = newPage
+}
+
+func (this *BrowserHistory) Back(steps int) string {
+	for steps > 0 && this.current.prev != nil {
+		this.current = this.current.prev
+		steps--
+	}
+
+	return this.current.url
+}
+
+func (this *BrowserHistory) Forward(steps int) string {
+	for steps > 0 && this.current.next != nil {
+		this.current = this.current.next
+		steps--
+	}
+
+	return this.current.url
+}
